a6a38e0aa00c875980672d5b5dbb65c6badf95bc
Unlink <miso250593@gmail.com>
2017-07-05 18:37:11 +0200

    Add project files.

---
+++ Countdown/ClockWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Countdown
+{
+    /// <summary>
+    /// Interaction logic for ClockWindow.xaml
+    /// </summary>
+    public partial class ClockWindow : Window
+    {
+
+        DispatcherTimer _timer;
+        TimeSpan _time;
+
+        public ClockWindow(TimeSpan cas, Color farba, int velkost, bool tien)
+        {
+            InitializeComponent();
+            MouseDown += Window_MouseDown;
+
+            _time = cas;
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                tbTime.Text = _time.ToString();
+                if (_time == TimeSpan.Zero) _timer.Stop();
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
+
+            tbTime.FontSize = velkost;
+            tbTime.Foreground = new SolidColorBrush(farba);
+            if (tien)
+            {
+                tbTime.Effect = new DropShadowEffect();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
---
+++ Countdown/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Countdown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ClockWindow _clockWindow;
+        private double lastTop = 0;
+        private double lastLeft = 0;
+        private bool wasShown = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 20; i < 250; i++)
+            {
+                Velkost.Items.Add(i);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int h, m, s;
+            try
+            {
+                var parsedTime = Cas.Text.Split(new[] { ':' }).Select(t => int.Parse(t)).ToArray();
+                if (parsedTime.Count() == 3)
+                {
+                    h = parsedTime[0];
+                    m = parsedTime[1];
+                    s = parsedTime[2];
+                }
+                else
+                {
+                    h = 0;
+                    m = parsedTime[0];
+                    s = parsedTime[1];
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(this, "Nesprávny formát času", "Error");
+                return;
+            }
+
+            TimeSpan cas = new TimeSpan(h,m,s);
+            Color farba = (Color) (Farba.SelectedColor ?? Colors.White);
+            int velkost = (int) Velkost.SelectedItem;
+            bool tien = (bool) (Tien.IsChecked ?? false);
+
+            if (_clockWindow != null)
+            {
+                lastTop = _clockWindow.Top;
+                lastLeft = _clockWindow.Left;
+                _clockWindow.Close();
+            }
+
+            _clockWindow = new ClockWindow(cas, farba, velkost, tien);
+            _clockWindow.Show();
+            if (wasShown)
+            {
+                _clockWindow.Top = lastTop;
+                _clockWindow.Left = lastLeft;
+            }
+            wasShown = true;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_clockWindow != null)
+            {
+                lastTop = _clockWindow.Top;
+                lastLeft = _clockWindow.Left;
+                _clockWindow.Close();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_clockWindow != null)
+            {
+                _clockWindow.Close();
+            }
+        }
+
+        private void Cas_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
